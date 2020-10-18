b628a823d0bb002711cf73def7dafbb98f9ab762
johngiorgi <johnmgiorgi@gmail.com>
2020-10-18 11:57:54 -0400

    :bookmark: Set version alpha

--- allennlp_multi_label/__init__.py
+++ allennlp_multi_label/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.0-alpha.0"
 
 from allennlp_multi_label.dataset_reader import MultiLabelTextClassificationJsonReader
 from allennlp_multi_label.model import MultiLabelClassifier
